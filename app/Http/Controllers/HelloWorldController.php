c7c8122751679279bf0b6471e0e17f6146661331
balloon.chen <balloon.chen@104.com.tw>
2020-03-27 21:41:58 +0800

    add HelloWorldController.php and hello_world.blade.php

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class HelloWorldController extends BaseController
+{
+    public function hello_world() {
+        return view('hello_world');
+    }
+}
