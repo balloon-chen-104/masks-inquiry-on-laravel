301077332db78148c73a633eb7fa46b34002a1b0
balloon.chen <balloon.chen@104.com.tw>
2020-03-31 10:53:48 +0800

    create CallApiException and add it to MasksInquiryController

--- app/Http/Controllers/MasksInquiryController.php
+++ app/Http/Controllers/MasksInquiryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Exceptions\CallApiException;
 
 class MasksInquiryController extends Controller
 {
@@ -12,14 +13,31 @@ class MasksInquiryController extends Controller
         if (!$area) {
             $area = "";
         }
-        $masksInquiryApi = "http://laravel.api/api/masks-inquiry-api?area=$area";
-        $dataJson = file_get_contents($masksInquiryApi);
-        
-        $recordDatas = json_decode($dataJson, true);
-        foreach ($recordDatas as &$recordData) {
-            $recordData = (array) $recordData;
-        }
 
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL, "http://laravel.api/api/masks-inquiry-api?area=$area");
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        $dataJson = curl_exec($curl_handle);
+        curl_close($curl_handle);
+
+        try {
+            if (isJson($dataJson) === false) {
+                throw new CallApiException('masks-inquiry-api');
+            }
+            $recordDatas = json_decode($dataJson, true);
+            foreach ($recordDatas as &$recordData) {
+                $recordData = (array) $recordData;
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+        
         return view('masks-inquiry', ['area' => $area, 'recordDatas' => $recordDatas]);
     }
+}
+
+// https://vector.cool/php-is-json/
+function isJson($string)
+{
+    return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
 }
